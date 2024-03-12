c2cde88454f323d1ab210d9c04efaf0feb7f121f
Imre Turcsan <turcsan.imi926@gmail.com>
2024-03-12 22:32:58 +0100

    replaced navbar, new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './components/Home/Home.js'
-import About from './components/About/About.js'
-// import Navbar from './components/Navbar/Navbar.js';
-import Admin from './components/Admin/Admin.js';
-import ResponsiveAppBar from './components/ResponsiveAppBar/ResponsiveAppbar.js';
+import Home from './components/Home/index.js'
+import About from './components/About/index.js'
+import Admin from './components/Admin/index.js';
+import ResponsiveAppBar from './components/ResponsiveAppBar/index.js';
+import Welcome from './components/Welcome/index.js';
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
     <>
     <ResponsiveAppBar/>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Welcome/>} />
         <Route path="/Home" element={<Home/>} />
         <Route path="/Admin" element={<Admin/>} />
         <Route path="/About" element={<About/>} />
